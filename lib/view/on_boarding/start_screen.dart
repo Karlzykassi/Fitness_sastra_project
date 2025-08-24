58f71e0533a1d1fbc26b7c4c7cb213681a0b121b
Pechsamnang <seanpechsamnang@gmail.com>
2025-08-24 19:26:45 +0700

    update start screen

---
+++ lib/view/on_boarding/start_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:fitness/common/Color/color.dart';
+
+class StartScreen extends StatefulWidget {
+  const StartScreen({super.key});
+
+  @override
+  State<StartScreen> createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Tcolor.primaryColor1,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                "Fitness",
+                style: TextStyle(
+                  color: Tcolor.secondaryColor,
+                  fontSize: 38,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 30),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                height: 50,
+                width: media.width * 0.7,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(),
+                  child: Text(
+                    "Start",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Tcolor.primaryColor1,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
